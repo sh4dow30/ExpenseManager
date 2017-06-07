bafeb8bfd332a65ed15d9f65043c7bcab83c9b13
Maciej S <Maciej S>
2017-06-07 21:23:42 +0200

    Add project files.

---
+++ SkiSoft.ExpenseManager.DataModel/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SkiSoft.ExpenseManager.DataModel
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        public string Description { get; set; }
+    }
+}
