bc285cea82b05d8a1ec1ba1c7360c844d3c79c9d
Maciej S <sh4dow30@gmail.com>
2017-06-07 22:19:57 +0200

    Test gui projects added.

---
+++ SkiSoft.ExpenseManager.AndroidGui/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using SkiSoft.ExpenseManager.DataModel;
+
+namespace SkiSoft.ExpenseManager.AndroidGui
+{
+    [Activity(Label = "SkiSoft.ExpenseManager.AndroidGui", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Account _account;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
